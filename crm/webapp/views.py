8291ddc573a0e9e1a69257a2eab5fe8891835782
Allard Scheers <allard.scheers@kpn.com>
2025-07-24 20:46:26 +0200

    Insert login, logout and registration

--- crm/webapp/views.py
+++ crm/webapp/views.py
@@ -1,5 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 
 def home(request):
-    return render(request, "home.html", {})
+    # Check to see if logging in
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        # Authenticate the user
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, "You have successfully logged in.")
+            return redirect("home")
+        else:
+            messages.success(request, "Invalid username or password.")
+            return redirect("home")
+    else:
+        return render(request, "home.html", {})
+
+
+def login_user(request):
+    pass
+
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, "You have successfully logged out.")
+    return redirect("home")
+
+
+def register_user(request):
+    return render(request, "register.html", {})
