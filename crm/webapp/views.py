bcb83ca7ce425373189eb1df18d72ab0c781271a
Allard Scheers <allard.scheers@kpn.com>
2025-07-24 21:09:36 +0200

    Update registration of a user

--- crm/webapp/views.py
+++ crm/webapp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from .forms import SignUpForm
 
 
 def home(request):
@@ -32,4 +33,20 @@ def logout_user(request):
 
 
 def register_user(request):
-    return render(request, "register.html", {})
+    if request.method == "POST":
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            # Automatically log in the user after registration
+            username = form.cleaned_data.get("username")
+            password = form.cleaned_data.get("password1")
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            # Show success message
+            messages.success(request, "You have successfully registered.")
+            return redirect("home")
+    else:
+        form = SignUpForm()
+        return render(request, "register.html", {"form": form})
+
+    return render(request, "register.html", {"form": form})
